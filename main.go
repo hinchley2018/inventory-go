4028ee17798506dc9f0b9773c4bb327dc1d01b36
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2021-12-24 23:50:33 -0600

    generate swagger docs

--- main.go
+++ main.go
@@ -1,13 +1,36 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	docs "github.com/hinchley2018/inventory-go/docs"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+// @BasePath /api/v1
+// @Summary list parts
+// @Accept json
+// @Produce json
+// @Success 200
+// @Router /categories [get]
+func GetPartCategories(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Hello",
+	})
+}
 
 func main() {
 	r := gin.Default()
-	r.GET("/list", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello",
-		})
-	})
-	r.Run()
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	v1 := r.Group("/api/v1")
+	{
+		categoriesGroup := v1.Group("/categories")
+		{
+			categoriesGroup.GET("/", GetPartCategories)
+		}
+	}
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+	r.Run(":8080")
 }
